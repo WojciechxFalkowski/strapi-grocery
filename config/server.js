1d6f309dc0d8a164e5974cf63b71a3de9172836b
Wojciech Falkowski <wojciech.falkowski@movecloser.pl>
2021-07-26 22:04:16 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '38631a965e569502736611efd82e6e07'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2fa4359f-022e-4075-bcfb-f1cf296a46a7'
+};
